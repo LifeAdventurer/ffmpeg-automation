cf5a6e996b8c7e7a7052b1a0927a60764e373e68
lifeadventurer <life0adventurer@gmail.com>
2024-07-01 00:10:04 +0800

    add convert.py

---
+++ convert.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def convert_opus_to_mp3(input_filepath, output_filepath):
+    command = [
+        "ffmpeg",
+        "-i",
+        input_filepath,
+        "-acodec",
+        "libmp3lame",
+        "-aq",
+        "4",
+        output_filepath,
+    ]
+    subprocess.run(command)
